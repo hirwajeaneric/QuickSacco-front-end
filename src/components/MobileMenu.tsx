ac782978e17b95baf107fe0fde560342c5cd1cbf
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-05-16 08:33:20 +0200

    Refactoring

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -4,16 +4,17 @@ type Props = {
     handleClose: () => void
 }
 
-const MobileMenu = ({handleClose}: Props) => {
+const MobileMenu = ({ handleClose }: Props) => {
     return (
         <>
             {/* Mobile Menu  */}
             <div className="md:hidden container fixed left-0 top-12 w-full bg-blue-500 text-white">
                 <span className="flex flex-col gap-5 py-10">
-                    <Link to={'/'} onClick={handleClose} className="text-white py-2 hover:bg-blue-500 focus:bg-blue-500">Home</Link>
-                    <Link to={'/'} onClick={handleClose} className="text-white py-2 hover:bg-blue-500 focus:bg-blue-500">Services</Link>
-                    <Link to={'/about'} onClick={handleClose} className="text-white py-2 hover:bg-blue-500 focus:bg-blue-500">About Us</Link>
-                    <Link to={'/apply'} onClick={handleClose} className="text-white py-2 hover:bg-blue-500 focus:bg-blue-500">Apply</Link>
+                    <a href={'/'} onClick={handleClose} className="text-white">Home</a>
+                    <a href={'#process'} onClick={handleClose} className="text-white">Process</a>
+                    <a href={'#benefits'} onClick={handleClose} className="text-white">Benefits</a>
+                    <Link to={'/apply'} onClick={handleClose} className=" text-white">Apply</Link>
+                    
                     <Link to={'/signin'} onClick={handleClose} className="text-white py-2 hover:bg-blue-500 focus:bg-blue-500">Get started</Link>
                 </span>
             </div>
