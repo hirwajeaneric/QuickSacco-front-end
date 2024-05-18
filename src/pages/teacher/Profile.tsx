9aaefdc84be53b0cdd02299c6ade7fec153683a2
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-05-18 11:44:48 +0200

    Feature: User Profile - In progress

--- src/pages/teacher/Profile.tsx
+++ src/pages/teacher/Profile.tsx
@@ -1,6 +1,19 @@
+import UserProfileForm from "@/components/forms/UserProfileForm"
+import { useState } from "react"
+
 const Profile = () => {
+  const [user, setuser] = useState({});
   return (
-    <div>Profile</div>
+    <section className="flex flex-col gap-4">
+      <div>
+        <h2 className='text-2xl font-bold'> User Profile Form</h2>
+        <UserProfileForm 
+          currentUser={user}
+          onSave={true}
+          isLoading={true}
+        />
+      </div>
+    </section>
   )
 }
 
---
+++ src/types.tsx
@@ -0,0 +1,7 @@
+export type User = {
+    _id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    phone: string;
+};
