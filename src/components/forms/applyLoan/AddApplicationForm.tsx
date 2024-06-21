44b4d4d7f5d1c6478a3bfaad1fb731a6d98717a8
hirwajeaneric <hirwajeaneric25@gmail.com>
2024-06-21 22:32:38 +0200

    Updates

--- src/components/forms/applyLoan/AddApplicationForm.tsx
+++ src/components/forms/applyLoan/AddApplicationForm.tsx
@@ -7,19 +7,27 @@ import { Outlet } from 'react-router-dom';
 import { calculatePaymentPeriod } from '@/utils/helperFunctions';
 
 const formSchema = z.object({
-    firstName: z.string({
-        required_error: "FirstName is required"
-    }).trim().min(1, 'First name is required').max(255, 'First name is too long'),
-    lastName: z.string().trim().min(1, 'Last name is required').max(255, 'Last name is too long'),
-    nationalId: z.string().trim().min(16, 'National ID must be 16 characters long').max(16, 'National ID must be 16 characters long'),
-    email: z.string({
-        required_error: "Email address is required"
-    }).email('Invalid email format').trim(),
-    teacherId: z.string().trim(),
-    phone: z.string().trim().min(10, 'Phone number must be 10 digits long'),
-    dateOfBirth: z.date({
-        required_error: "Date of birth is required"
-    }),
+    firstName: z.string({ required_error: "FirstName is required" })
+        .trim()
+        .min(1, 'First name is required')
+        .max(255, 'First name is too long'),
+    lastName: z.string()
+        .trim()
+        .min(1, 'Last name is required')
+        .max(255, 'Last name is too long'),
+    nationalId: z.string()
+        .trim()
+        .min(16, 'National ID must be 16 characters long')
+        .max(16, 'National ID must be 16 characters long'),
+    email: z.string({ required_error: "Email address is required" })
+        .email('Invalid email format')
+        .trim(),
+    teacherId: z.string()
+        .trim(),
+    phone: z.string()
+        .trim()
+        .min(10, 'Phone number must be 10 digits long'),
+    dateOfBirth: z.date({ required_error: "Date of birth is required" }),
     gender: z.enum(['Male', 'Female', 'Other']), // Use enum for valid gender options
     maritalStatus: z.enum(['Single', 'Married', 'Divorced', 'Widowed']), // Use enum for valid marital status options
     numberOfDependencies: z.number({
@@ -28,34 +36,45 @@ const formSchema = z.object({
     }),
     workSchool: z.string({
         required_error: "Work school is required"
-    }).trim(),
+    })
+        .trim(),
     position: z.string({
         required_error: "Position is required"
-    }).trim(),
+    })
+        .trim(),
     monthlySalary: z.number({
         coerce: true,
         required_error: "Monthly salary is required"
-    }).positive('Monthly salary must be a positive number'),
-    amountToPayPerMonth: z.number().positive('Monthly payment must be a positive number'),
+    })
+        .positive('Monthly salary must be a positive number'),
+    amountToPayPerMonth: z.number()
+        .positive('Monthly payment must be a positive number'),
     amountRequested: z.number({
         coerce: true,
         required_error: "The requested amount is required"
-    }).positive('Amount requested must be a positive number'),
-    repaymentPeriod: z.number().positive('Repayment period must be a positive number'),
+    })
+        .positive('Amount requested must be a positive number'),
+    repaymentPeriod: z.number()
+        .positive('Repayment period must be a positive number'),
     suggestedRepaymentPeriod: z.number({
         coerce: true,
         required_error: "You must provide the time you estimate to pay back is required"
-    }).positive('Repayment period must be a positive number'),
+    })
+        .positive('Repayment period must be a positive number'),
     bankAccountNumber: z.string({
         required_error: "Bank account number is required"
-    }).trim(),
+    })
+        .trim(),
     proofOfEmployment: z.string({
         required_error: "Proof of employment is required"
-    }).trim(),
+    })
+        .trim(),
     copyOfNationalId: z.string({
         required_error: "Copy of national ID is required"
-    }).trim(),
-    comment: z.string().trim(),
+    })
+        .trim(),
+    comment: z.string()
+        .trim(),
 });
 
 // Determining the type of our form data by infering it from the zod schema 
